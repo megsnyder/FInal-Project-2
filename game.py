262bd91659bb04d5af1c46728b21873154902745
megsnyder <42872006+megsnyder@users.noreply.github.com>
2019-04-23 10:23:10 -0400

    Updated from Brython Server: 4/23/2019 10:23:09 AM

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+class Creature(Sprite):
+    
+myapp = Game(SCREEN_WIDTH,SCREEN_HEIGHT)
+myapp.run()
