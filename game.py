20439dfb3af67f1e87c84c60c37f1ba62462dd39
megsnyder <42872006+megsnyder@users.noreply.github.com>
2019-04-30 10:04:57 -0400

    Updated from Brython Server: 4/30/2019 10:04:57 AM

--- game.py
+++ game.py
@@ -4,6 +4,18 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
 class Creature(Sprite):
+    def __init__(self, position):
+        super().__init__(Player.asset, position)
+        
+        self.vx = 0
+        self.vy = 0
+class Game(App):
     
+    def __init__(self,width,height):
+        bg_asset = RectangleAsset(self.width, self.height, noline, winter)
+        ground_asset = RectangleAsset(self.width, 400, noline, white)
+        bg = Sprite(bg_asset, (0,0))
+        ground = Sprite(ground_asset, (0,400))
+
 myapp = Game(SCREEN_WIDTH,SCREEN_HEIGHT)
 myapp.run()
