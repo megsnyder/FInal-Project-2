76d236bf1e3470da77414b02473824f4852b0089
megsnyder <42872006+megsnyder@users.noreply.github.com>
2019-05-03 10:40:38 -0400

    Updated from Brython Server: 5/3/2019 10:40:37 AM

--- fun.py
+++ fun.py
@@ -4,11 +4,10 @@ import random
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
 
-red = Color(0xd90000, 1.0)
+grey = Color(0xd90000, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xFFFFFF, 1.0)
-brown = Color(0xa07251, 1.0)
-green1 = Color(0x3ddb33, 1.0)
+green2 = Color(0x3ddb33, 1.0)
 
 noline = LineStyle(0, white)
 
