c7a66d68b66a4d226a797732dc2aea6e3788bf20
megsnyder <42872006+megsnyder@users.noreply.github.com>
2019-05-03 10:24:01 -0400

    Updated from Brython Server: 5/3/2019 10:24:01 AM

--- food.py
+++ food.py
@@ -8,13 +8,13 @@ red = Color(0xd90000, 1.0)
 black = Color(0x000000, 1.0)
 white = Color(0xFFFFFF, 1.0)
 brown = Color(0xa07251, 1.0)
-green = Color(0x3ddb33, 1.0)
+green1 = Color(0x3ddb33, 1.0)
 
 noline = LineStyle(0, white)
 
 apple=CircleAsset(10,noline,red)
 stem=RectangleAsset(2,7,noline,brown)
-leaf=EllipseAsset(5,2,noline,green)
+leaf=EllipseAsset(5,2,noline,green1)
 shine=CircleAsset(5,noline,white)
 shadow=CircleAsset(7,noline,red)
 
