30d9259ac2b9b644edf348f6469cafcf10ddee7e
megsnyder <42872006+megsnyder@users.noreply.github.com>
2019-05-07 10:34:23 -0400

    Updated from Brython Server: 5/7/2019 10:34:23 AM

--- game.py
+++ game.py
@@ -240,6 +240,12 @@ class Game(App):
         if self.creature1.visible==True and self.f<500 and self.f<self.s and self.f<self.p:
             self.creature1sad=Creature1sad((297,295))
             self.creature1.visible=False
+        if self.creature1.visible==True and self.s<500 and self.s<self.f and self.f<self.p:
+            self.creature1tired=Creature1tired((297,295))
+            self.creature1.visible=False
+        if self.creature1.visible==True and self.p<500 and self.p<self.s and self.p<self.f:
+            self.creature1bored=Creature1bored((297,295))
+            self.creature1.visible=False
         if self.f==0 or self.s==0 or self.p==0:
             print("game over")
 myapp = Game(SCREEN_WIDTH,SCREEN_HEIGHT)
